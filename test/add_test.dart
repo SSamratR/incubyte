aea19e8bcc65238f0335baeb154acc68c8711ae4
SSamratR <smrutisamratransingh@gmail.com>
2025-12-29 21:27:26 +0530

    Add initial test setup for string addition functionality

---
+++ test/add_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {
+    // Code to run before each test
+  });
+
+  tearDown(() {
+    // Code to run after each test
+  });
+
+  test('String addition of an empty string will return 0', () {});
+}
