574600cd642d966bab890d3116aa63b02295be60
SSamratR <smrutisamratransingh@gmail.com>
2025-12-29 22:09:28 +0530

    Refactor : cleanup

--- test/add_test.dart
+++ test/add_test.dart
@@ -2,14 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:incubyte/extension_add.dart';
 
 void main() {
-  setUp(() {
-    // Code to run before each test
-  });
-
-  tearDown(() {
-    // Code to run after each test
-  });
-
   test('String addition of an empty string will return 0', () {
     const input = '';
     final result = input.add();
