7309173706241dee84617ecaa6739c558aa284a1
SSamratR <smrutisamratransingh@gmail.com>
2025-12-29 21:46:29 +0530

    Green : Add necessary if clause to handle string addition of number like entries

--- lib/extension_add.dart
+++ lib/extension_add.dart
@@ -2,6 +2,8 @@ extension StringCalculatorExtension on String {
   int add() {
     if (isEmpty) {
       return 0;
+    } else if (int.tryParse(this) != null) {
+      return int.parse(this);
     }
     return length;
   }
