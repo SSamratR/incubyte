b8925f7e0efbefc0ecad978018402f7a6c575314
SSamratR <smrutisamratransingh@gmail.com>
2025-12-29 22:08:53 +0530

    Green : Add necessary if clause to correctly handle addition of comma-separated numbers in String extension

--- lib/extension_add.dart
+++ lib/extension_add.dart
@@ -4,7 +4,11 @@ extension StringCalculatorExtension on String {
       return 0;
     } else if (int.tryParse(this) != null) {
       return int.parse(this);
+    } else if (contains(',') && split(',').isNotEmpty) {
+      final numbers = split(',').map((e) => int.parse(e)).toList();
+      return numbers.reduce((a, b) => a + b);
     }
+
     return length;
   }
 }
